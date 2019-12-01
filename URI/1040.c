34cd5e609e9e753a3343bb824e79581cd6035e8d
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2019-12-01 22:00:46 +0600

    Add files via upload

---
+++ URI/1040.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    double a, b, c, d, last, m;
+    scanf("%lf%lf%lf%lf", &a, &b, &c, &d);
+    m = (a * 2 + b * 3 + c * 4 + d) / 10;
+
+    printf("Media: %.1f\n", m);
+
+    if (m >= 7.0)
+        {
+           printf("Aluno aprovado.\n");
+        }
+    else if (m >= 5.0)
+    {
+        printf("Aluno em exame.\n");
+        scanf("%lf", &last);
+
+        printf("Nota do exame: %.1f\n", last);
+
+        if (last + m / 2.0 > 5.0)
+            {
+                printf("Aluno aprovado.\n");
+            }
+        else
+           {
+                printf("Aluno reprovado.\n");
+           }
+        printf("Media final: %.1f\n", (last + m ) / 2.0);
+    }
+    else
+        {
+             printf("Aluno reprovado.\n");
+        }
+
+    return 0;
+}
