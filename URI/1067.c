0216c730d5ec24a20f4e1051bee30f131ce49bb9
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-02-22 21:31:57 +0600

    Add files via upload

---
+++ URI/1067.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x, i;
+    scanf("%d", &x);
+
+    for(i = 0; i <= x; ++i)
+    {
+        if(i % 2 == 1)
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
