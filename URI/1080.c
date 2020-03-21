4a2e1f9c1a364e15b2af712c03a975e9256c3eb8
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-03-21 20:15:02 +0600

    Add files via upload

---
+++ URI/1080.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+ int n, max = -1, p, i;
+ for (i = 1; i < 101; ++i)
+ {
+  scanf("%d", &n);
+  if(max < n)
+   max = n, p = i;
+ }
+
+ printf("%d\n%d\n", max, p);
+
+ return 0;
+}
