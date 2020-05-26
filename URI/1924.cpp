585d1d3b0b40a05958134acaf72d23fa3b646206
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-05-26 09:29:04 +0600

    Add files via upload

---
+++ URI/1924.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, b;
+    char a[100];
+    cin>>n;
+    {
+        for(b=1; b<=n; b++)
+            cin>>a;
+
+        cout<<"Ciencia da Computacao"<<endl;
+    }
+    return 0;
+}
